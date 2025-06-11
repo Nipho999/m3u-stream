659f54ca34581d14dc8f1666fad7081d0d171cfd
Mr6teenx <85907637+Nipho999@users.noreply.github.com>
2025-06-11 14:34:08 +0200

    Update app-4.py

--- app-4.py
+++ app-4.py
@@ -89,4 +89,4 @@ def play_channel(channel_id):
                            current_time=datetime.now().strftime("%Y-%m-%d %H:%M"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
